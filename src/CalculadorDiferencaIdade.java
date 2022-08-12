72983a8107784bf4daf27b04d84258e0a749f153
silviobegoti <begoti@hotmail.com>
2022-08-12 12:52:57 -0300

    Initial commit

---
+++ src/CalculadorDiferencaIdade.java
@@ -0,0 +1,8 @@
+public class CalculadorDiferencaIdade {
+    public static void main(String[] args) {
+        int primeiraIdade = 50;
+        int segundaIdade = 15;
+        int diferencaIdade = primeiraIdade - segundaIdade;
+        System.out.println("A diferença entre idades é: "+ diferencaIdade);
+    }
+}
