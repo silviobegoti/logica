a38e5f426022f7ba41b45c75d386ef9d445ba845
silviobegoti <begoti@hotmail.com>
2022-08-15 12:15:12 -0300

    Calcular porcentagem salário

---
+++ src/CalculadoraImposto.java
@@ -0,0 +1,15 @@
+/*
+Crie uma classe que calcule uma determninada porcentagem de um dado salário
+Criar uma classe
+Aceitar um valor de entrada para salário
+Definir o valor da porcentagem (perguntar quanto cliente?) = 30%
+Calcular  a porcentagem
+ */
+public class CalculadoraImposto {
+    public static void main(String[] args){
+        float salario = 2500.00F;
+        float porcentagem = 30;
+        float porcentagemDoSalario = salario * (porcentagem / 100);
+        System.out.println(porcentagemDoSalario);
+    }
+}
