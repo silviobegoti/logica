d7807cd54240902a8c36afac17d11fe69543f2bd
silviobegoti <begoti@hotmail.com>
2022-08-17 13:32:47 -0300

    Reutilizando Variáveis

---
+++ src/ReutilizandoVariaveis.java
@@ -0,0 +1,22 @@
+/*
+Crie uma variável que irá guradar o valor de um salário;
+Calcule a porcentagem desse salário, os valores da porcentagem
+serão:
+*30%
+*15%
+*5%
+A cada vez que você calcular guarde o resultado em uma variável
+Imprima o resultado e reutilize a variável que guarde o resultado
+para o novo cálculo, repetindo o processo
+*/
+public class ReutilizandoVariaveis {
+    public static void main(String[] args){
+        double salario = 5000;
+        double resultado = salario * 0.3;
+        System.out.println("30% eh: "+resultado);
+        resultado = salario * 0.15;
+        System.out.println("15% eh: "+resultado);
+        resultado = salario * 0.05;
+        System.out.println("5% eh: "+resultado);
+    }
+}
