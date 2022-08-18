96a37aaecfb9938f9286e6ac7f6777f6d18b6cfb
silviobegoti <begoti@hotmail.com>
2022-08-18 12:03:28 -0300

    Condicionais IF e ELSE

---
+++ src/VerificaIdade.java
@@ -0,0 +1,15 @@
+/*
+Receba uma idade como entrada
+Se idade for maior que 18 imprima "Adulto"
+Senão imrima "Ainda Não é Adulto"
+*/
+public class VerificaIdade {
+    public static void main(String[] args) {
+        int idade = 15;
+        if (idade > 18) {
+            System.out.println("Adulto");
+        } else {
+            System.out.println("Ainda Não é Adulto");
+        }
+    }
+}
