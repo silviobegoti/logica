68fa1001a6c23e64ae94e5190f9220ea86e90743
silviobegoti <begoti@hotmail.com>
2022-08-18 12:51:18 -0300

    Concatenar

---
+++ src/ImparOuPar.java
@@ -0,0 +1,17 @@
+/*
+16
+Dado um determinado número inteiro
+imprima se ele é impar ou par
+Resto da divisão por 2 se for par sempre vai ser 0
+Senão vai ser impar
+*/
+public class ImparOuPar {
+    public static void main(String[] args) {
+        int numero = 6;
+        if ((numero % 2) == 0) {
+            System.out.println("PAR: " + (numero % 2));
+        } else {
+            System.out.println("IMPAR: " + (numero % 2));
+        }
+    }
+}
