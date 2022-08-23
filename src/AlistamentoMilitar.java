0971acfaf7cc408479960a406190437eadd6c76f
silviobegoti <begoti@hotmail.com>
2022-08-23 12:45:51 -0300

    Entrada de Dados do Teclado

---
+++ src/AlistamentoMilitar.java
@@ -0,0 +1,15 @@
+public class AlistamentoMilitar {
+    public static void main(String[] args) {
+        char sexo = 'M';
+        int idade = 18;
+
+        if ((sexo == 'M' || sexo == 'F') && idade < 18) {
+            System.out.println("Alistamento não permitido!!!");
+        } else if (sexo == 'M' && idade >= 18) {
+            System.out.println("Alistamento Obrigatório!!!");
+        } else if (sexo == 'F' && idade >= 18) {
+            System.out.println("Deseja se alistar?");
+        }
+    }
+}
+
